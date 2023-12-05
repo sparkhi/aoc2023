ce33dd6baf9e599e8587f525eaf225b00bbb972f
sparkhi <parkhi@gmail.com>
2023-12-05 18:33:59 +0000

    Day 4 - Part 1 complete

---
+++ src/main/scala/com/knowingwhere/aoc2023/Day4.scala
@@ -0,0 +1,36 @@
+package com.knowingwhere.aoc2023
+
+import scala.io.Source
+
+object Day4 extends App{
+
+  val lines = Source.fromResource("day4-input.txt").getLines().toList
+  val scratchcards = getScratchcards(lines)
+
+  //part1
+  println(scratchcards.map(_.score).sum)
+
+  def createScratchcard(cardline: String):Scratchcard = {
+    val idAndNumbers = cardline.split(":")
+    val id = idAndNumbers.head.substring("Card ".length).trim.toInt
+    val numbers = idAndNumbers.tail.head.split("\\|")
+    val winningNumbers = ("""\d+""".r findAllIn numbers.head).map(_.toInt).toList
+    val chosenNumbers = ("""\d+""".r findAllIn numbers.tail.head).map(_.toInt).toList
+    Scratchcard(id, winningNumbers, chosenNumbers)
+  }
+
+  def getScratchcards(lines: List[String]) = {
+    val scratchcards = lines.map(createScratchcard)
+    scratchcards
+  }
+}
+
+case class Scratchcard (Id: Int, winningNumbers: List[Int], chosenNumbers: List[Int]) {
+  def score: Int = {
+    val commonNumbers = winningNumbers.intersect(chosenNumbers).size
+    if (commonNumbers == 0)
+      0
+    else
+      Math.pow(2, commonNumbers - 1).toInt
+  }
+}
---
+++ src/test/scala/com/knowingwhere/aoc2023/Day4Test.scala
@@ -0,0 +1,22 @@
+package com.knowingwhere.aoc2023
+
+import org.scalatest.Matchers._
+import org.scalatest._
+
+import scala.io.Source
+class Day4Test extends WordSpec with BeforeAndAfterEach {
+  "Day 4" should {
+    val lines = Source.fromResource("day4-test-input.txt").getLines().toList
+    "create correct scratchcard " in {
+      val scratchcard = Day4.createScratchcard("Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1")
+      scratchcard.Id shouldBe 3
+      scratchcard.winningNumbers should contain allElementsOf List(1, 21, 53, 59, 44)
+      scratchcard.chosenNumbers should contain allElementsOf List(69, 82, 63, 72, 16, 21, 14,  1)
+      scratchcard.score shouldBe 2
+
+      Day4.createScratchcard("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53").score shouldBe 8
+      Day4.createScratchcard("Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83").score shouldBe 1
+      Day4.createScratchcard("Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11").score shouldBe 0
+    }
+  }
+}
