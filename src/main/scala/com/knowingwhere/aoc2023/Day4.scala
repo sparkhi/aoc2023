3614fdda9a5cfb0657c774a41c88011d61512f42
sparkhi <parkhi@gmail.com>
2023-12-06 13:14:56 +0000

    Day4 part 2 done

--- src/main/scala/com/knowingwhere/aoc2023/Day4.scala
+++ src/main/scala/com/knowingwhere/aoc2023/Day4.scala
@@ -1,5 +1,8 @@
 package com.knowingwhere.aoc2023
 
+import java.util
+import scala.annotation.tailrec
+import scala.collection.mutable
 import scala.io.Source
 
 object Day4 extends App{
@@ -9,6 +12,38 @@ object Day4 extends App{
 
   //part1
   println(scratchcards.map(_.score).sum)
+
+  //part 2 - find total number of cards
+  val scratchcardCountMap = collection.mutable.Map(scratchcards.map(_ -> 1): _*)
+  processScratchCards(scratchcardCountMap, 1)
+
+  println(scratchcardCountMap.values.toList.sum)
+
+  //FIXME - "Unit" - not happy with it but for now its okay
+  @tailrec
+  def IncrementCardCountForNext(scratchcardCountMap: mutable.Map[Scratchcard, Int], currentCardId: Int, score: Int, currentCardCopiesCount: Int): Unit = {
+    val currentCardOption = scratchcardCountMap.keySet.find(_.Id == currentCardId)
+    if (score > 0 && currentCardOption.isDefined) {
+      val currentCard = currentCardOption.get
+      scratchcardCountMap(currentCard) += currentCardCopiesCount
+      IncrementCardCountForNext(scratchcardCountMap, currentCardId + 1, score - 1, currentCardCopiesCount)
+    }
+  }
+
+  @tailrec
+  def processScratchCards(scratchcardCountMap: mutable.Map[Scratchcard, Int], currentCardId: Int): mutable.Map[Scratchcard, Int] = {
+    val currentCardOption = scratchcardCountMap.keySet.find(_.Id == currentCardId)
+    if (currentCardOption.isDefined) {
+      val currentCard = currentCardOption.get
+      val score = currentCard.matchingNumberCount
+      val currentCardCopiesCount = scratchcardCountMap(currentCard)
+      IncrementCardCountForNext(scratchcardCountMap, currentCardId + 1, score, currentCardCopiesCount)
+      processScratchCards(scratchcardCountMap, currentCardId + 1)
+    } else {
+      scratchcardCountMap
+    }
+  }
+
 
   def createScratchcard(cardline: String):Scratchcard = {
     val idAndNumbers = cardline.split(":")
--- src/test/scala/com/knowingwhere/aoc2023/Day4Test.scala
+++ src/test/scala/com/knowingwhere/aoc2023/Day4Test.scala
@@ -18,5 +18,12 @@ class Day4Test extends WordSpec with BeforeAndAfterEach {
       Day4.createScratchcard("Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83").score shouldBe 1
       Day4.createScratchcard("Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11").score shouldBe 0
     }
+
+    "part 2 should increment the scratch card numbers" in {
+      val scratchcards = Day4.getScratchcards(lines)
+      val scratchcardCountMap = collection.mutable.Map(scratchcards.map(_ -> 1): _*)
+      val answer = Day4.processScratchCards(scratchcardCountMap, 1)
+      println(answer.values.toList.sum)
+    }
   }
 }
