402b2ca6abe9945bb8fa16b08eccb83ad890aeab
sparkhi <parkhi@gmail.com>
2023-12-28 17:50:01 +0000

    Day 11 Part 1 complete

--- src/main/scala/com/knowingwhere/aoc2023/Day11.scala
+++ src/main/scala/com/knowingwhere/aoc2023/Day11.scala
@@ -1,7 +1,6 @@
 package com.knowingwhere.aoc2023
 
 import com.knowingwhere.aoc2023.util.Position
-
 import scala.annotation.tailrec
 import scala.io.Source
 
@@ -12,6 +11,54 @@ object Day11 extends App {
 
   // Part 1
   println(findAllPathDistanceSum(galaxies, BigInt(0)))
+
+  /*
+    For Part 1, we had expanded the universe and did the path distance counting, however that method would not finish
+    in case of Part 2 when the universe is expanding a million times (i.e. each blank row / col is replaced by a million
+    blank rows or columns ... which in turn means the path calculation will need to add 999999 new blank rows or columns
+    for each corresponding blank row or column
+
+    We have kept the calculation routine from the part 1 as it is and added new methods so that we can have a reference
+    to what we had initially done later stage if needed
+   */
+
+  //Part2 - don't use expanded universe, of lines that need to expand.
+  val rowsToExpand: List[Int] = compactUniverse.zipWithIndex.filter(!_._1.contains('#')).map(_._2)
+  val colsToExpand: List[Int] = compactUniverse.transpose.zipWithIndex.filter(!_._1.contains('#')).map(_._2)
+  val compactGalaxies = compactUniverse.zipWithIndex.flatMap(rowEntry => createGalaxyLocationsOfRow(rowEntry._2, rowEntry._1))
+  println(findAllPathDistanceSumPart2(compactGalaxies, 1000000, rowsToExpand, colsToExpand, BigInt(0)))
+
+
+  @tailrec
+  def findAllPathDistanceSumPart2(remainingGalaxies: List[Position], expansionFactor: Int, rowsToExpand: List[Int], colsToExpand: List[Int], distanceSum: BigInt): BigInt= {
+    if (remainingGalaxies.isEmpty) {
+      distanceSum
+    } else {
+      val currentStart = remainingGalaxies.head
+      val allDistanceSum = getDistancesPart2(currentStart, remainingGalaxies.tail, expansionFactor, rowsToExpand, colsToExpand, distanceSum)
+      findAllPathDistanceSumPart2(remainingGalaxies.tail, expansionFactor, rowsToExpand, colsToExpand, allDistanceSum)
+    }
+  }
+
+  @tailrec
+  def getDistancesPart2(fromGalaxy: Position, remainingGalaxies: List[Position], expansionFactor:Int, rowsToExpand: List[Int], colsToExpand: List[Int], runningSum: BigInt): BigInt = {
+    if (remainingGalaxies.isEmpty) {
+      runningSum
+    } else {
+      val toGalaxy = remainingGalaxies.head
+      val smallX = Math.min(fromGalaxy.x, toGalaxy.x)
+      val bigX = Math.max(fromGalaxy.x, toGalaxy.x)
+      val smallY = Math.min(fromGalaxy.y, toGalaxy.y)
+      val bigY = Math.max(fromGalaxy.y, toGalaxy.y)
+      val colCountToExpand = colsToExpand.count(colId => colId > smallX && colId < bigX)
+      val rowCountToExpand = rowsToExpand.count(rowId => rowId > smallY && rowId < bigY)
+
+      // each row or column is replaced by the number of rows or columns in expansionFactor, so the number of ADDITIONAL
+      // ROWS or COLUMNS is (expansionFactor - 1)
+      val distance = (bigX - smallX) + (bigY - smallY) + colCountToExpand * (expansionFactor - 1) + rowCountToExpand * (expansionFactor - 1)
+      getDistancesPart2(fromGalaxy, remainingGalaxies.tail, expansionFactor, rowsToExpand: List[Int], colsToExpand: List[Int], runningSum + distance)
+    }
+  }
 
   @tailrec
   def findAllPathDistanceSum(remainingGalaxies: List[Position], distanceSum: BigInt): BigInt= {
@@ -35,7 +82,6 @@ object Day11 extends App {
     }
   }
 
-
   def createGalaxyLocationsOfRow(rowIndex: Int, rowString: String): List[Position] = {
     createGalaxyLocations(0, rowIndex, rowString, List.empty)
   }
@@ -53,7 +99,6 @@ object Day11 extends App {
       }
     }
   }
-
 
   def expand(compactUniverse: List[String]) = {
     val rowsToExpand: List[Int] = compactUniverse.zipWithIndex.filter(!_._1.contains('#')).map(_._2)
--- src/test/scala/com/knowingwhere/aoc2023/Day11Test.scala
+++ src/test/scala/com/knowingwhere/aoc2023/Day11Test.scala
@@ -6,6 +6,26 @@ import org.scalatest._
 class Day11Test extends WordSpec with BeforeAndAfterEach{
   "Day 11" should {
     "expand the universe where there are no galaxies present" in {
+      val galaxyMap =
+        """...#......
+          |.......#..
+          |#.........
+          |..........
+          |......#...
+          |.#........
+          |.........#
+          |..........
+          |.......#..
+          |#...#.....""".stripMargin
+
+      val compactUniverse = galaxyMap.split("\n").toList
+
+      val universe = Day11.expand(compactUniverse)
+      val galaxies = universe.zipWithIndex.flatMap(rowEntry => Day11.createGalaxyLocationsOfRow(rowEntry._2, rowEntry._1))
+      galaxies.size shouldBe 9
+      Day11.findAllPathDistanceSum(galaxies, BigInt(0)) shouldBe 374
+    }
+    "keep compact universe and add rows and cols to distance" in {
       val galaxyMap = """...#......
                         |.......#..
                         |#.........
@@ -19,10 +39,14 @@ class Day11Test extends WordSpec with BeforeAndAfterEach{
 
       val compactUniverse = galaxyMap.split("\n").toList
 
-      val universe = Day11.expand(compactUniverse)
-      val galaxies = universe.zipWithIndex.flatMap(rowEntry => Day11.createGalaxyLocationsOfRow(rowEntry._2, rowEntry._1))
+      //val universe = Day11.expand(compactUniverse)
+      val galaxies = compactUniverse.zipWithIndex.flatMap(rowEntry => Day11.createGalaxyLocationsOfRow(rowEntry._2, rowEntry._1))
+      val rowsToExpand: List[Int] = compactUniverse.zipWithIndex.filter(!_._1.contains('#')).map(_._2)
+      val colsToExpand: List[Int] = compactUniverse.transpose.zipWithIndex.filter(!_._1.contains('#')).map(_._2)
       galaxies.size shouldBe 9
-      Day11.findAllPathDistanceSum(galaxies, BigInt(0)) shouldBe 374
+      Day11.findAllPathDistanceSumPart2(galaxies, 2, rowsToExpand, colsToExpand, BigInt(0)) shouldBe 374
+      Day11.findAllPathDistanceSumPart2(galaxies, 10, rowsToExpand, colsToExpand, BigInt(0)) shouldBe 1030
+      Day11.findAllPathDistanceSumPart2(galaxies, 100, rowsToExpand, colsToExpand, BigInt(0)) shouldBe 8410
     }
   }
 }
