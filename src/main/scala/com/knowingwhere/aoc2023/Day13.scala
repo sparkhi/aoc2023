8df8da5a08dbc6705f51939774e2705eaa955ffa
sparkhi <parkhi@gmail.com>
2024-01-04 11:50:41 +0000

    Day 13 finished, for part 2 no code change comitted but a comment to mention the one line change

--- src/main/scala/com/knowingwhere/aoc2023/Day13.scala
+++ src/main/scala/com/knowingwhere/aoc2023/Day13.scala
@@ -14,30 +14,44 @@ object Day13 extends App {
   println(verticalLinesOfSymmetry.sum + horizontalLinesOfSymmetry.map(_ * 100).sum)
 
   @tailrec
-  def isValidMirror(block: List[String], currentCandidate: Int, matchIndexSmall: Int, matchIndexBig: Int, isValidSoFar: Boolean): Boolean = {
-    if ((matchIndexSmall < 0) || (matchIndexBig == block.size)) {
-      isValidSoFar
+  def getDiffCount(string1: String, string2: String, runningCount: Int): Int = {
+    if (string1.length != string2.length) throw new IllegalArgumentException()
+
+    if (string1.isEmpty) {
+      runningCount
     } else {
-      if (!isValidSoFar) {
-        isValidSoFar
+      val char1 = string1.head
+      val char2 = string2.head
+      if (char1 == char2) {
+        getDiffCount(string1.tail, string2.tail, runningCount)
       } else {
-        val stringsWithIndex = block.zipWithIndex
-        val string1 = stringsWithIndex.filter(_._2 == matchIndexSmall).head._1
-        val string2 = stringsWithIndex.filter(_._2 == matchIndexBig).head._1
-        if (string1 == string2) {
-          isValidMirror(block, currentCandidate, matchIndexSmall - 1, matchIndexBig + 1, isValidSoFar)
-        } else {
-          isValidMirror(block, currentCandidate, matchIndexSmall - 1, matchIndexBig + 1, isValidSoFar = false)
-        }
+        getDiffCount(string1.tail, string2.tail, runningCount + 1)
       }
     }
   }
+
+  @tailrec
+  def validMirrorDiffCounter(block: List[String], currentCandidate: Int, matchIndexSmall: Int, matchIndexBig: Int, runningDiffCounter: Int): Boolean = {
+    if ((matchIndexSmall < 0) || (matchIndexBig == block.size)) {
+      //at termination of the recursion, if the running diff is 0, it means all comparisons are exact mirrors,
+      // for part 2 of the problem, the running difference need to be exactly 1 (smudge),
+      // so change the following line to compare with 1 instead of 0 to get answer for part 2
+      runningDiffCounter == 0
+    } else {
+      val stringsWithIndex = block.zipWithIndex
+      val string1 = stringsWithIndex.filter(_._2 == matchIndexSmall).head._1
+      val string2 = stringsWithIndex.filter(_._2 == matchIndexBig).head._1
+      val diffCount = getDiffCount(string1, string2, 0)
+      validMirrorDiffCounter(block, currentCandidate, matchIndexSmall - 1, matchIndexBig + 1, runningDiffCounter + diffCount)
+    }
+  }
+
   @tailrec
   def findLinesOfSymmetry(block: List[String], currentCandidate: Int, accumulatedLines: List[Int]): List[Int] = {
     if (currentCandidate == block.size) {
       accumulatedLines
     } else {
-      if (isValidMirror(block, currentCandidate, currentCandidate - 1, currentCandidate, isValidSoFar = true)) {
+      if (validMirrorDiffCounter(block, currentCandidate, currentCandidate - 1, currentCandidate, 0)) {
         findLinesOfSymmetry(block, currentCandidate + 1, accumulatedLines :+ currentCandidate)
       } else {
         findLinesOfSymmetry(block, currentCandidate + 1, accumulatedLines)
